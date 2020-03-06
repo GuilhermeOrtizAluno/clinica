ad2953717ef7565a79321d76bef538ab2d3d5970
gsantos.1999@alunos.utfpr.edu.br <a2058944@pg-lab5-wx17.PGROSSA>
2020-03-06 11:12:14 -0300

    Entidade procedimento

---
+++ src/entidades/Procedimentos.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author a2058944
+ */
+public class Procedimentos {
+    private int codigo, numSessoes;
+    private double valor;
+    private String descricao;
+    private StringBuffer orientacoes;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getNumSessoes() {
+        return numSessoes;
+    }
+
+    public void setNumSessoes(int numSessoes) {
+        this.numSessoes = numSessoes;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StringBuffer getOrientacoes() {
+        return orientacoes;
+    }
+
+    public void setOrientacoes(StringBuffer orientacoes) {
+        this.orientacoes = orientacoes;
+    }
+    
+}
